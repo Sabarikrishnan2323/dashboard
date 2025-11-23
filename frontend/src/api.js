158fd83b81ab68af916c981e5df2d0a0366995d7
sabarikrishnan2323 <sabarikrishnan712@gmail.com>
2025-11-23 23:44:24 +0530

    vite url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dashboard-backend-pv96.onrender.com",
+  baseURL: "https://dashboard-backend-pv96.onrender.com/api/",
 });
 
 export default API;
